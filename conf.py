34c3349b5126030e83848e9e0f7880f7940e12f1
socialibrary <wasimprasad@gmail.com>
2012-01-01 01:55:28 +0530

    first commit

---
+++ conf.py
@@ -0,0 +1,30 @@
+# Copyright 2011 Facebook, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# Facebook Application ID and Secret.
+FACEBOOK_APP_ID = '187138028036281'
+FACEBOOK_APP_SECRET = '5de92f0074bb5451f68f991df3a87723'
+
+# Canvas Page name.
+FACEBOOK_CANVAS_NAME = 'librarium'
+
+# A random token for use with the Real-time API.
+FACEBOOK_REALTIME_VERIFY_TOKEN = 'RANDOM TOKEN'
+
+# The external URL this application is available at where the Real-time API will
+# send it's pings.
+EXTERNAL_HREF = 'http://socialibrary.appspot.com/'
+
+# Facebook User IDs of admins. The poor mans admin system.
+ADMIN_USER_IDS = ['517160144']
